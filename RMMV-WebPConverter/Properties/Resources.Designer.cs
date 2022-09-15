d3c96be27984671b7dec474ffe7ff1b1fdae5248
Dimitris Trachiotis <acemod13@outlook.com>
2022-09-15 13:23:34 +0300

    Update for .NET 7.

--- RMMV-WebPConverter/Properties/Resources.Designer.cs
+++ RMMV-WebPConverter/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace RMMVWebPConverter.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -239,7 +239,7 @@ namespace RMMVWebPConverter.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to = Version R1.00 ({0}).
+        ///   Looks up a localized string similar to = Version R1.01 ({0}).
         /// </summary>
         internal static string ProgramVersion {
             get {
